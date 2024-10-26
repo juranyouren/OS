bba2fd5db991c0680782145f028eba404f9d304c
yslyyddl <458180583@qq.com>
2024-10-26 14:15:31 +0800

    Update memlayout.h

--- lab2/kern/mm/memlayout.h
+++ lab2/kern/mm/memlayout.h
@@ -54,11 +54,19 @@ struct Page {
 #define le2page(le, member)                 \
     to_struct((le), struct Page, member)
 
+#define MAX_BUDDY_ORDER 20
+
 /* free_area_t - maintains a doubly linked list to record free (unused) pages */
 typedef struct {
     list_entry_t free_list;         // the list header
     unsigned int nr_free;           // number of free pages in this free list
 } free_area_t;
+
+typedef struct {
+    unsigned int max_order;                           // 伙伴二叉树的层数
+    list_entry_t free_array[MAX_BUDDY_ORDER + 1];     // 链表数组(现在默认有14层，即2^14 = 16384个可分配物理页)，每个数组元素都一个free_list头
+    unsigned int nr_free;                             // 伙伴系统中剩余的空闲块
+} free_buddy_t;
 
 #endif /* !__ASSEMBLER__ */
 
