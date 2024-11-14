f178e4bfa2d2f90ea7251e64328ffba72b551ec3
李昕杨 <129027825+juranyouren@users.noreply.github.com>
2024-11-14 15:59:13 +0800

    Update swap_clock.c

--- lab3/kern/mm/swap_clock.c
+++ lab3/kern/mm/swap_clock.c
@@ -38,6 +38,10 @@ _clock_init_mm(struct mm_struct *mm)
      // 初始化当前指针curr_ptr指向pra_list_head，表示当前页面替换位置为链表头
      // 将mm的私有成员指针指向pra_list_head，用于后续的页面替换算法操作
      //cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
+     list_init(&pra_list_head);
+     curr_ptr = &pra_list_head;
+     mm->sm_priv = &pra_list_head;
+     cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
      return 0;
 }
 /*
@@ -52,8 +56,12 @@ _clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, in
     //record the page access situlation
     /*LAB3 EXERCISE 4: YOUR CODE*/ 
     // link the most recent arrival page at the back of the pra_list_head qeueue.
+    list_entry_t *head = (list_entry_t*) mm->sm_priv;
+    list_add(head->prev, entry);
     // 将页面page插入到页面链表pra_list_head的末尾
+    page->visited = 1;
     // 将页面的visited标志置为1，表示该页面已被访问
+    
     return 0;
 }
 /*
@@ -73,9 +81,26 @@ _clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tic
         /*LAB3 EXERCISE 4: YOUR CODE*/ 
         // 编写代码
         // 遍历页面链表pra_list_head，查找最早未被访问的页面
+        while(curr_ptr==head){
+    		curr_ptr = curr_ptr->next;
+    	}
         // 获取当前页面对应的Page结构指针
+        struct Page *tmp;
+        list_entry_t *miduse;
+        tmp = le2page(curr_ptr,pra_page_link);
         // 如果当前页面未被访问，则将该页面从页面链表中删除，并将该页面指针赋值给ptr_page作为换出页面
+        if(tmp->visited == 0){
+            miduse = curr_ptr;
+            curr_ptr = curr_ptr->next;
+            list_del(miduse);
+            *ptr_page = tmp;
+            break;
+        }
         // 如果当前页面已被访问，则将visited标志置为0，表示该页面已被重新访问
+        else{
+            tmp->visited = 0;
+            curr_ptr = curr_ptr->next;
+        }
     }
     return 0;
 }
