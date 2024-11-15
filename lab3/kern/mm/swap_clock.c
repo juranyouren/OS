49327c6e81e30312143a4044272f544a6dfb05c2
李昕杨 <129027825+juranyouren@users.noreply.github.com>
2024-11-15 10:40:13 +0800

    Update swap_clock.c

--- lab3/kern/mm/swap_clock.c
+++ lab3/kern/mm/swap_clock.c
@@ -41,7 +41,7 @@ _clock_init_mm(struct mm_struct *mm)
      list_init(&pra_list_head);
      curr_ptr = &pra_list_head;
      mm->sm_priv = &pra_list_head;
-     cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
+     //cprintf("curr_ptr %x",curr_ptr);
      return 0;
 }
 /*
@@ -61,7 +61,7 @@ _clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, in
     // 将页面page插入到页面链表pra_list_head的末尾
     page->visited = 1;
     // 将页面的visited标志置为1，表示该页面已被访问
-    cprintf("curr_ptr %x in fifo_init_mm\n",curr_ptr);
+    
     return 0;
 }
 /*
@@ -83,7 +83,9 @@ _clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tic
         // 遍历页面链表pra_list_head，查找最早未被访问的页面
         while(curr_ptr==head){
     		curr_ptr = curr_ptr->next;
+            cprintf("curr_ptr 0xffffffff%x\n",curr_ptr);
     	}
+        cprintf("curr_ptr 0xffffffff%x\n",curr_ptr);
         // 获取当前页面对应的Page结构指针
         struct Page *tmp;
         list_entry_t *miduse;
@@ -93,6 +95,8 @@ _clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tic
             miduse = curr_ptr;
             curr_ptr = curr_ptr->next;
             list_del(miduse);
+            
+            
             *ptr_page = tmp;
             break;
         }
