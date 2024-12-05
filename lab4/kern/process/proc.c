9d483771c137e14a8e700f66a20db1a7bd30bdec
Mi-nku <2274394870@qq.com>
2024-12-05 14:28:36 +0800

    exercise 2

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -292,7 +292,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    //LAB4:EXERCISE2 YOUR CODE
+    //LAB4:EXERCISE2 2211448
     /*
      * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
      * MACROs or Functions:
@@ -311,13 +311,32 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
      */
 
     //    1. call alloc_proc to allocate a proc_struct
+    proc = alloc_proc();
+    if (proc == NULL) {
+        goto fork_out;
+    }
     //    2. call setup_kstack to allocate a kernel stack for child process
+    ret = setup_kstack(proc);
+    if (ret != 0) {
+        goto bad_fork_cleanup_proc;
+    }
     //    3. call copy_mm to dup OR share mm according clone_flag
+     ret = copy_mm(clone_flags, proc);
+    if (ret != 0) {
+        goto bad_fork_cleanup_kstack;
+    }
     //    4. call copy_thread to setup tf & context in proc_struct
+    copy_thread(proc, stack, tf);
     //    5. insert proc_struct into hash_list && proc_list
+    proc->pid = get_pid();
+    hash_proc(proc);
+    list_add(&proc_list, &(proc->list_link));
+    nr_process ++;
     //    6. call wakeup_proc to make the new child process RUNNABLE
+    
+    wakeup_proc(proc);
     //    7. set ret vaule using child proc's pid
-
+    ret = proc->pid;
     
 
 fork_out:
